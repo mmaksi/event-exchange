4f40eae08c70bea5462e754ed6ed07115edf9e31
Mark Maksi <mmaksi.dev@gmail.com>
2024-09-23 11:56:59 +0200

    created a common module for middlewares and error handling for all services

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/forbidden-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './errors/server-error';
+export * from './errors/too-many-requests-error';
+export * from './errors/unauthorized-error';
+
+export * from './middlewares/error-handler.middleware';
+export * from './middlewares/require-auth.middleware';
+export * from './middlewares/security.middleware';
+export * from './middlewares/current-user.middleware';
+export * from './middlewares/request-validator.middleware';
